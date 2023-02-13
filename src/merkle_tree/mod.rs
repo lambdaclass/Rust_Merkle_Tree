d64057b6762bcd587a90e47c0cf2ac5131e83fc3
matias-gonz <maigonzalez@fi.uba.ar>
2023-02-13 16:27:00 -0300

    refactor: MerkleTree::new now builds the nodes

--- src/merkle_tree/mod.rs
+++ src/merkle_tree/mod.rs
@@ -7,21 +7,22 @@ pub struct MerkleTree {
 
 impl MerkleTree {
     pub fn new(elements: Vec<String>) -> MerkleTree{
-        let hashes = elements.iter().map(|e| hash(e.to_string())).collect();
+        let leaves = elements.iter().map(|e| hash(e.to_string())).collect();
+        let hashes = Self::build_hashes(leaves);
         MerkleTree{hashes, count: elements.len()}
     }
 
     pub fn root_hash(tree: MerkleTree) -> Vec<u8> {
-        Self::get_root_hash(tree.hashes)
+        tree.hashes[0].clone()
     }
 
-    fn get_root_hash(hashes: Vec<Vec<u8>>) -> Vec<u8> {
+    fn build_hashes(hashes: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
         if hashes.len() == 1 {
-            return hashes[0].clone();
+            return hashes;
         }
 
-        let h = hashes.chunks(2).map(|e| hash_pair(e[0].clone(), e[1].clone())).collect();
-        Self::get_root_hash(h)
+        let h: Vec<Vec<u8>>= hashes.chunks(2).map(|e| hash_pair(e[0].clone(), e[1].clone())).collect();
+        [Self::build_hashes(h.clone()), h].concat()
     }
 }
 
